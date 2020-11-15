ad906c16e2db05caafdf9f92a4ee3aa7b99e01bc
EslamGalal <eslamgalal077@gmail.com>
2020-11-15 15:15:38 +0200

    edited post

--- pages/post.js
+++ pages/post.js
@@ -7,8 +7,7 @@ function Post(props) {
   return (
     <Layout >
       <h1>{title}</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-    Incidunt, blanditiis!</p>
+      <p style={{ width: "80vw" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate repellendus labore nobis quis dolores dolorem veritatis rem iusto accusantium suscipit tenetur maiores, debitis dicta odio autem doloribus asperiores fugiat in tempore. Quod veritatis exercitationem reiciendis sit voluptatibus? Provident, voluptas quo?</p>
     </Layout>
   )
 }
