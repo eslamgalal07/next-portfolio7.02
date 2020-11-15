fa850b02501edbd248c8199f819c335979abb03b
EslamGalal <eslamgalal077@gmail.com>
2020-11-15 15:44:49 +0200

    edited post

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,9 @@
 import Layout from '../components/layout';
 import Link from 'next/link';
 
-const PostLink = ({ title, slug }) => (
+const PostLink = ({ title }) => (
  <li>
-  <Link as={`/${slug}`} href={`/post?title=${title}`}>
+  <Link href={`/post?title=${title}`}>
    <a>{`${title} Post`}</a>
   </Link>
  </li>
@@ -13,9 +13,9 @@ function Blog(props) {
  return (
   <Layout title='Blog'>
    <ul>
-    <PostLink slug='react-post' title='React Post' />
-    <PostLink slug='Angular-post' title='Angular Post' />
-    <PostLink slug='vue-post' title='Vue Post' />
+    <PostLink title='React Post' />
+    <PostLink title='Angular Post' />
+    <PostLink title='Vue Post' />
    </ul>
   </Layout>
  )
